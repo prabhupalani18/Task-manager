8157e24ad0529b900e58cd20e7fff0cc80849ea4
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2023-07-22 13:51:46 +0530

    Adding users and tasks

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+require("./db/mongoose")
+const User = require("./models/users")
+const Tasks = require("./models/tasks")
+
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+app.post('/users', (req,res)=>{
+    const user = new User(req.body)
+    user.save().then(()=>{
+        res.send(user)
+    }).catch((error)=>{
+        res.status(400).send(error)
+    })
+})
+
+app.post('/tasks', (req,res)=>{
+    const task = new Tasks(req.body)
+    task.save().then(()=>{
+        res.send(task)
+    }).catch((error)=>{
+        res.status(400).send(error)
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running at port ${port}`)
+})
