b44726ce4eacde420f5eab2e4744b7dc8ef1638b
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2023-07-27 15:03:29 +0530

    Error handling for file upload middleware

--- src/routers/users.js
+++ src/routers/users.js
@@ -118,6 +118,8 @@ router.delete('/users/me', auth, async(req,res)=>{
 
 router.post('/users/me/avatar', upload.single('avatar'), (req,res)=>{
     res.send()
+},(error,req,res,next)=>{
+        return res.status(400).send({"error": error.message})
 })
 
 module.exports = router
