4aadc952a68f71914793663359e5fa74c1ef3e32
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2023-07-28 01:53:57 +0530

    Added try catch

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,21 +4,31 @@ const sgMailApiKey = process.env.SENDGRID_API_KEY
 sgMail.setApiKey(sgMailApiKey)
 
 const sendWelcomeEmail = async(email, name)=>{
-    sgMail.send({
-        to: email,
-        from: 'sriprabhu642.sp@gmail.com',
-        subject: 'Thanks for joingin in!',
-        text: `Welcome to the app, ${name}, let us know how you get along with the application`
-    })
+    try{
+        sgMail.send({
+            to: email,
+            from: 'sriprabhu642.sp@gmail.com',
+            subject: 'Thanks for joingin in!',
+            text: `Welcome to the app, ${name}, let us know how you get along with the application`
+        })
+    }catch(error){
+        throw new Error(error)
+    }
+    
 }
 
 const sendCancellationEmail = async(email, name)=>{
-    sgMail.send({
-        to: email,
-        from: 'sriprabhu642.sp@gmail.com',
-        subject: 'Sorry to see you go!',
-        text: `Goodbye, ${name}. Is there anything we could have done to keep you onboard, please let us know \n I hope to see you back sometime soon`
-    })
+    try{
+        sgMail.send({
+            to: email,
+            from: 'sriprabhu642.sp@gmail.com',
+            subject: 'Sorry to see you go!',
+            text: `Goodbye, ${name}. Is there anything we could have done to keep you onboard, please let us know \n I hope to see you back sometime soon`
+        })
+    }catch(error){
+        throw new Error(error)
+    }
+    
 }
 
 module.exports = {
